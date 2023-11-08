e393069793b344cf78c7cbc9db48b1473aeac7a4
Aaron King <aaron@aaronjamesking.com>
2023-11-08 08:12:49 -0500

    Fix: Prevent "window is not defined" error during SSR in Node environments

--- src/lib/hooks/useOnlineStatus.ts
+++ src/lib/hooks/useOnlineStatus.ts
@@ -6,7 +6,11 @@ import { uuidv4 } from '../../utils/uuid';
 import { LoggerInterface } from '../Logger';
 
 function useOnlineStatus(sdk: SendbirdChat, logger: LoggerInterface) {
-  const [isOnline, setIsOnline] = useState(window?.navigator?.onLine ?? true);
+  const [isOnline, setIsOnline] = useState(
+    typeof window !== "undefined"
+      ? (window?.navigator?.onLine ?? true)
+      : true
+  );
 
   useEffect(() => {
     const uniqueHandlerId = uuidv4();
